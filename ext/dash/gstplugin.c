c80853dc81f100af3b92e4a1acd954c9e3e35ec5
void <void@guardz.ru>
2017-08-08 16:54:01 +0300

    dashsink was added

--- ext/dash/gstplugin.c
+++ ext/dash/gstplugin.c
@@ -7,16 +7,22 @@
 #include <gst/gst.h>
 
 #include "gstdashdemux.h"
+#include "gstdashsink.h"
 
 static gboolean
 dashdemux_init (GstPlugin * plugin)
 {
-  return gst_element_register (plugin, "dashdemux", GST_RANK_PRIMARY,
-      GST_TYPE_DASH_DEMUX);
+  if (!gst_element_register (plugin, "dashdemux", GST_RANK_PRIMARY,
+          GST_TYPE_DASH_DEMUX))
+    return FALSE;
+  if (!gst_element_register (plugin, "dashsink", GST_RANK_SECONDARY,
+          GST_TYPE_DASH_SINK))
+    return FALSE;
+  return TRUE;
 }
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     dashdemux,
-    "DASH demuxer plugin",
+    "DASH plugin",
     dashdemux_init, VERSION, "LGPL", PACKAGE_NAME, GST_PACKAGE_ORIGIN)
